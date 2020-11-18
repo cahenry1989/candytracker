c42ffbe3f1182cdaaa6b0b3098908267dc133e20
cahenry1989 <cahenry1989@gmail.com>
2020-11-17 22:17:13 -0600

    updated prod enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjzwBMqGcMhjbWcTwj-7EN8KwfsTe6sRA",
+    authDomain: "candyhousetracker.firebaseapp.com",
+    databaseURL: "https://candyhousetracker.firebaseio.com",
+    projectId: "candyhousetracker",
+    storageBucket: "candyhousetracker.appspot.com",
+    messagingSenderId: "62897972470",
+    appId: "1:62897972470:web:334155e813ad87c9b5f8d7",
+    measurementId: "G-FX4P03WGXF"
+    }
+  };
